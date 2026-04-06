def0823264bf81143a88095a9a6df9dfde789f14
dircs <sayminho383@gmail.com>
2026-04-06 14:45:10 -0300

    Implement HTTP server with save/load functionality

---
+++ main.py
@@ -0,0 +1,58 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json, os
+
+TOKEN = 'korone_place_491391_server'
+DATA_FOLDER = 'data'
+
+if not os.path.exists(DATA_FOLDER):
+	os.makedirs(DATA_FOLDER)
+
+class Handler(BaseHTTPRequestHandler):
+
+	def do_POST(self):
+		if self.headers.get('Authorization') != TOKEN:
+			self.send_response(403)
+			self.end_headers()
+			return
+
+		length = int(self.headers['Content-Length'])
+		body = self.rfile.read(length)
+		data = json.loads(body)
+
+		if self.path == '/save':
+			with open(f"{DATA_FOLDER}/{data['UserId']}.json", 'w') as f:
+				json.dump(data['Data'], f)
+
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(b'{"status":"saved"}')
+
+		elif self.path == '/load':
+			path = f"{DATA_FOLDER}/{data['UserId']}.json"
+
+			if os.path.exists(path):
+				with open(path) as f:
+					response = f.read()
+			else:
+				response = json.dumps({"Coins":0,"Blocks":[]})
+
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(response.encode())
+
+		elif self.path == '/debug':
+			print('DEBUG RECEBIDO:', data)
+
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(b'{"status":"ok"}')
+
+		else:
+			self.send_response(404)
+			self.end_headers()
+
+port = int(os.environ.get('PORT', 3000))
+server = HTTPServer(('0.0.0.0', port), Handler)
+
+print('rodando...')
+server.serve_forever()
